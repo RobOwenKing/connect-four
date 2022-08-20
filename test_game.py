52d46cabb2f02609bc26d888cef2223c64beec9e
Robert King <robert.o.king@hotmail.co.uk>
2022-08-20 18:11:27 +0100

    test: Second __init__() test case to cover different board sizes

--- test_game.py
+++ test_game.py
@@ -6,8 +6,14 @@ import game
 
 
 class TestBoard(TestCase):
-    def test_init(self):
+    def test_init_narrow_board(self):
         with StringIO() as output, redirect_stdout(output):
-            board = game.Board(3, 3)
+            board = game.Board(2, 3)
             board.print()
-            self.assertEqual(output.getvalue(), " 1 2 3\n| | | |\n| | | |\n| | | |\n")
+            self.assertEqual(output.getvalue(), " 1 2\n| | |\n| | |\n| | |\n")
+
+    def test_init_wide_board(self):
+        with StringIO() as output, redirect_stdout(output):
+            board = game.Board(3, 2)
+            board.print()
+            self.assertEqual(output.getvalue(), " 1 2 3\n| | | |\n| | | |\n")
