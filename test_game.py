2d947469e418c5c34da896e7dc05504b8ca947bd
Robert King <robert.o.king@hotmail.co.uk>
2022-08-23 17:41:35 +0100

    refactor: Change test_init...s to allow for introduction of minimum dimensions

--- test_game.py
+++ test_game.py
@@ -8,15 +8,21 @@ import game
 class TestBoard(TestCase):
     def test_init_narrow_board(self):
         with StringIO() as output, redirect_stdout(output):
-            board = game.Board(2, 3)
+            board = game.Board(4, 5)
             board.print()
-            self.assertEqual(output.getvalue(), " 1 2\n| | |\n| | |\n| | |\n")
+            self.assertEqual(
+                output.getvalue(),
+                " 1 2 3 4\n| | | | |\n| | | | |\n| | | | |\n| | | | |\n| | | | |\n",
+            )
 
     def test_init_wide_board(self):
         with StringIO() as output, redirect_stdout(output):
-            board = game.Board(3, 2)
+            board = game.Board(5, 4)
             board.print()
-            self.assertEqual(output.getvalue(), " 1 2 3\n| | | |\n| | | |\n")
+            self.assertEqual(
+                output.getvalue(),
+                " 1 2 3 4 5\n| | | | | |\n| | | | | |\n| | | | | |\n| | | | | |\n",
+            )
 
     def test_single_move(self):
         with StringIO() as output, redirect_stdout(output):
