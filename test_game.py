236e05a1efdbbd6035942e75ae1fcf0bd95b6f2b
Robert King <robert.o.king@hotmail.co.uk>
2022-08-23 17:46:02 +0100

    refactor: All tests to allow for introduction fo minimum Board dimensions

--- test_game.py
+++ test_game.py
@@ -26,38 +26,44 @@ class TestBoard(TestCase):
 
     def test_single_move(self):
         with StringIO() as output, redirect_stdout(output):
-            board = game.Board(3, 3)
+            board = game.Board(4, 4)
             board.move("x", 1)
             board.print()
-            self.assertEqual(output.getvalue(), " 1 2 3\n| | | |\n| | | |\n|x| | |\n")
+            self.assertEqual(
+                output.getvalue(),
+                " 1 2 3 4\n| | | | |\n| | | | |\n| | | | |\n|x| | | |\n",
+            )
 
     def test_multiple_moves(self):
         with StringIO() as output, redirect_stdout(output):
-            board = game.Board(3, 3)
+            board = game.Board(4, 4)
             board.move("x", 1)
             board.move("o", 1)
             board.move("x", 1)
             board.move("o", 2)
             board.print()
-            self.assertEqual(output.getvalue(), " 1 2 3\n|x| | |\n|o| | |\n|x|o| |\n")
+            self.assertEqual(
+                output.getvalue(),
+                " 1 2 3 4\n| | | | |\n|x| | | |\n|o| | | |\n|x|o| | |\n",
+            )
 
     def test_full_column_error(self):
-        board = game.Board(3, 2)
-        board.move("x", 1)
-        board.move("o", 1)
+        board = game.Board(4, 4)
+        for i in range(4):
+            board.move("x", 1)
         with self.assertRaises(ValueError):
             board.move("x", 1)
 
     def test_invalid_piece(self):
-        board = game.Board(2, 2)
+        board = game.Board(4, 4)
         with self.assertRaises(ValueError):
             board.move("a", 1)
 
     def test_invalid_column(self):
-        board = game.Board(2, 2)
+        board = game.Board(4, 4)
         with self.assertRaises(ValueError):
             board.move("x", 0)
         with self.assertRaises(ValueError):
-            board.move("x", 3)
+            board.move("x", 5)
         with self.assertRaises(ValueError):
-            board.move("x", 1.5)
+            board.move("x", 2.5)
