d3aadaebfa719dc7b0da0ebc20930a15496aea7d
Robert King <robert.o.king@hotmail.co.uk>
2022-09-05 14:43:42 +0100

    feat: Start Board.is_game_over() covering is_draw() case

--- game.py
+++ game.py
@@ -4,16 +4,34 @@ class Board:
         self.width = self.set_dimension(width)
         self.board = [[" " for i in range(self.width)] for j in range(self.height)]
         self.players = ["x", "o"]
+        self.last_placement = ["", ""]
 
     def print(self):
         print(" " + " ".join([str(x) for x in range(1, self.width + 1)]))
         for row in self.board:
             print("|" + "".join([x + "|" for x in row]))
 
+    def is_won(self):
+        print("Win!")
+
+    def is_draw(self):
+        if all([s.strip() for s in self.board[0]]):
+            print("Game over! You drew.")
+            return True
+        return False
+
+    def is_game_over(self):
+        if self.is_won():
+            return True
+        elif self.is_draw():
+            return True
+        return False
+
     def attempt_move(self, piece, column):
         for j in range(self.height - 1, -1, -1):
             if self.board[j][column - 1] == " ":
                 self.board[j][column - 1] = piece
+                self.last_placement = [column - 1, j]
                 """ self.print() """
                 return True
         raise ValueError("Error: Column {} is already full".format(column))
@@ -50,6 +68,7 @@ class Game:
             self.board.move(self.players[self.current_player], col)
             self.board.print()
             self.current_player = 1 - self.current_player
+            self.game_over = self.board.is_game_over()
         except Exception as e:
             print(e)
 
